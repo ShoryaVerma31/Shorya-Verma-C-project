0869888b75965de97d08d9ac7019615b3e5a2dc3
Shorya Verma <shoryaverma3105@gmail.com>
2025-11-20 22:01:24 +0530

    project

--- project_code.c
+++ project_code.c
@@ -13,3 +13,18 @@ typedef struct {
     char name[50];
     enum Status status;
 } Passenger;
+
+Passenger confirmed[MAX_SEATS];
+Passenger waiting[MAX_WAITING];
+
+int gentrate_PNR(){
+    return pnr++;
+}
+
+void Book_Tickect(char name[]){
+    Passenger p;
+    p.pnr = genrate_PNR();
+    strcpy(p.name, name);
+}
+
+    
