05f4a7ed5c69d3741fbbf3675b468b50935fbe92
Shorya Verma <shoryaverma3105@gmail.com>
2025-11-21 19:06:59 +0530

    project

--- project_code.c
+++ project_code.c
@@ -16,15 +16,39 @@ typedef struct {
 
 Passenger confirmed[MAX_SEATS];
 Passenger waiting[MAX_WAITING];
+int confirmedCount = 0, waitingCount = 0;
+int next_pnr = 1001;
 
 int gentrate_PNR(){
-    return pnr++;
+    return next_pnr++;
+}
+
+int seatsAvailable() {
+    return MAX_SEATS - confirmedCount;
 }
 
 void Book_Tickect(char name[]){
     Passenger p;
     p.pnr = genrate_PNR();
     strcpy(p.name, name);
+
+     if(confirmedCount < MAX_SEATS) {
+        p.status = CONFIRMED;
+        confirmed[confirmedCount++] = p;
+        printf("Ticket Booked Successfully!\n");
+    } 
+    else if(waitingCount < MAX_WAITING) {
+        p.status = WAITING;
+        waiting[waitingCount++] = p;
+        printf("Train Full .Added to waiting list.\n");
+    } 
+    else{
+        printf("Sorry! No seat and waiting list also full.\n");
+        return;
+    }
+    printf("Your PNR: %d\n", p.pnr);
 }
 
+
     
+
